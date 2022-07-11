43bd2ed89b20a1efcf0c0da4c00657aca5c0c0f1
Domino731 <d.orzechowski18@gmail.com>
2022-07-11 19:24:06 +0200

    jumping ball

--- Headers/Ball.h
+++ Headers/Ball.h
@@ -10,27 +10,23 @@ public:
     Ball(float x, float y);
     Ball() = delete;
     ~Ball() = default;
-    string name = "ASAS";
+
+    // update
+    void update();
+
+    // get positon of ball
+    float top();
+    float bot();
+    float left();
+    float right();
 
 private:
     CircleShape shape;
     const float ballRadius{10.1f};
+    const float ballVelocity{3.0f};
+
+    // 2 means 2 vectors
+    Vector2f velocity{ballVelocity, ballVelocity};
 
     void draw(RenderTarget &target, RenderStates state) const override;
 };
-// class Ball
-// {
-// public:
-//     Ball();
-//     // Ball(float t_x, float t_y);
-//     // Ball() = delete;
-//     // ~Ball() = default;
-
-//     string name = "cos";
-
-// private:
-//     CircleShape shape;
-//     const float ballRadius{10.1f};
-
-//     // void draw(RenderTarget &target, RenderStates state) const override;
-// };
--- main.cpp
+++ main.cpp
@@ -10,27 +10,29 @@ using namespace sf;
 int main()
 {
 
-    Ball ball(400, 400);
+    Ball ball(400, 300);
 
-    RenderWindow window{VideoMode{800, 800}, "Magic tree"};
+    RenderWindow window{VideoMode{800, 600}, "Magic tree"};
 
     window.setFramerateLimit(60);
 
     Event event;
-    // while (true)
-    // {
-    //     window.clear(Color::Black);
-    //     window.pollEvent(event);
+    while (true)
+    {
+        window.clear(Color::Black);
+        window.pollEvent(event);
 
-    //     if (event.type == Event::Closed)
-    //     {
-    //         window.close();
-    //         break;
-    //     }
-    //     window.draw(ball);
-    //     window.display();
-    // }
+        if (event.type == Event::Closed)
+        {
+            window.close();
+            break;
+        }
+        ball.update();
 
+        window.draw(ball);
+        window.display();
+    };
+    cout << "TEST";
     return 0;
 };
 
@@ -45,4 +47,46 @@ Ball::Ball(float x, float y)
 void Ball::draw(RenderTarget &target, RenderStates state) const
 {
     target.draw(this->shape, state);
+}
+
+void Ball::update()
+{
+    shape.move(this->velocity);
+
+    if (this->left() < 0)
+    {
+        velocity.x = ballVelocity;
+    }
+    else if (this->right() > 800)
+    {
+        velocity.x = -ballVelocity;
+    }
+    if (this->top() < 0)
+    {
+        velocity.y = ballVelocity;
+    }
+    else if (this->bot() > 600)
+    {
+        velocity.y = -ballVelocity;
+    }
+}
+
+float Ball::top()
+{
+    return shape.getPosition().y - shape.getRadius();
+}
+
+float Ball::bot()
+{
+    return shape.getPosition().y + shape.getRadius();
+}
+
+float Ball::left()
+{
+    return shape.getPosition().x - shape.getRadius();
+}
+
+float Ball::right()
+{
+    return shape.getPosition().x + shape.getRadius();
 }
