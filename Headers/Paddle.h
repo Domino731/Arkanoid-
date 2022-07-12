5ec72643fb2f407a4a8734d80dabe47b28817806
Domino731 <d.orzechowski18@gmail.com>
2022-07-12 21:00:08 +0200

    paddle

---
+++ Headers/Paddle.h
@@ -0,0 +1,30 @@
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+using namespace sf;
+
+class Paddle : public sf::Drawable
+{
+public:
+    Paddle(float x, float y);
+    Paddle() = delete;
+    ~Paddle() = default;
+
+    void update();
+    // get positon of ball
+    float top();
+    float bot();
+    float left();
+    float right();
+
+private:
+    RectangleShape shape;
+
+    float const paddle_width{80.0f};
+    float const paddle_height{20.0f};
+    float const paddle_velocity{6.0f};
+
+    Vector2f velocity{paddle_velocity, 0.f};
+
+    void draw(RenderTarget &target, RenderStates state) const override;
+};
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cstdlib>
 #include "Headers/Ball.h"
+#include "Headers/Paddle.h"
 
 using namespace std;
 using namespace sf;
@@ -11,10 +12,11 @@ int main()
 {
 
     Ball ball(400, 300);
+    Paddle paddle(400, 500);
 
     RenderWindow window{VideoMode{800, 600}, "Magic tree"};
 
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(75);
 
     Event event;
     while (true)
@@ -28,14 +30,18 @@ int main()
             break;
         }
         ball.update();
+        paddle.update();
 
         window.draw(ball);
+        window.draw(paddle);
+
         window.display();
     };
     cout << "TEST";
     return 0;
 };
 
+/// Ball classs
 Ball::Ball(float x, float y)
 {
     shape.setPosition(x, y);
@@ -89,4 +95,56 @@ float Ball::left()
 float Ball::right()
 {
     return shape.getPosition().x + shape.getRadius();
+}
+
+// Paddle class
+Paddle::Paddle(float x, float y)
+{
+    shape.setPosition(x, y);
+    shape.setSize({this->paddle_width, this->paddle_height});
+    shape.setFillColor(Color::Green);
+    shape.setOrigin(paddle_width / 2.f, paddle_height / 2.f);
+}
+
+void Paddle::draw(RenderTarget &target, RenderStates state) const
+{
+    target.draw(this->shape, state);
+}
+
+void Paddle::update()
+{
+    shape.move(this->velocity);
+
+    if (Keyboard::isKeyPressed(Keyboard::Key::Left) && this->left() > 0)
+    {
+        velocity.x = -paddle_velocity;
+    }
+    else if (Keyboard::isKeyPressed(Keyboard::Key::Right) && this->right() < 800)
+    {
+        velocity.x = paddle_velocity;
+    }
+    else
+    {
+        velocity.x = 0;
+    }
+}
+
+float Paddle::top()
+{
+    return shape.getPosition().y - shape.getSize().y / 2.f;
+}
+
+float Paddle::bot()
+{
+    return shape.getPosition().y + shape.getSize().y / 2.f;
+}
+
+float Paddle::left()
+{
+    return shape.getPosition().x - shape.getSize().x / 2.f;
+}
+
+float Paddle::right()
+{
+    return shape.getPosition().x + shape.getSize().x / 2.f;
 }
